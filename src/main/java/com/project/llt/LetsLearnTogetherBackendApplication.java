ee111c49c6843010bab5ef5d3325b7558a21a545
George-Bacalu <b.george1401@gmail.com>
2023-05-05 12:22:40 +0300

    Initial commit

---
+++ src/main/java/com/project/llt/LetsLearnTogetherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.llt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsLearnTogetherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsLearnTogetherBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/llt/LetsLearnTogetherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.llt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsLearnTogetherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
